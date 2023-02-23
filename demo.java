fd179b3ae8a694fd6f917e2aff440093adb720db
shabana <shabanamujawar41@gmail.com>
2023-02-23 10:37:26 +0000

    demo 3 added to feature1

--- demo.java
+++ demo.java
@@ -1,4 +1,7 @@
 public class demo {
 	 public static void main (string arg[)
+		 // print statement hello world
 		 system.out.println("hellow world")
+		 //print welcome
+                system.out.printin("welcome")
 }
